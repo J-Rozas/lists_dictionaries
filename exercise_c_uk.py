67493b01dedc25bf14625b827ad8a702eaad5b05
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-03-29 16:46:23 +0100

    Complete part 4 of exercise_c

--- exercise_c_uk.py
+++ exercise_c_uk.py
@@ -31,3 +31,6 @@ for country in united_kingdom:
   print(country["name"])
 
 # 4. Use a loop to find the total population of the UK.
+total_population = 0
+for country in united_kingdom:
+  total_population += country["population"]
